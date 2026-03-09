1e35c849190e1e79dbb1ba7339ca04def766426e
Tasibaev Davron <tdav@mail.ru>
2026-03-10 01:19:18 +0500

    refactor Component options handling

--- server/public/js/Components/Form/Component.js
+++ server/public/js/Components/Form/Component.js
@@ -1,24 +1,42 @@
 export default class Component {
-    constructor(parent, storeKey, store, prop, inputType, values, locked, options) {
+    constructor(options) {
+        const {
+            parent,
+            storeKey,
+            store,
+            prop,
+            inputType,
+            values,
+            locked,
+            elementOptions = {}
+        } = options || {};
+
         this.parent = parent;
         this.storeKey = storeKey;
         this.store = store;
-
         this.prop = prop;
-        this.name = `${prop}`.toLowerCase();
+        this.name = prop ? `${prop}`.toLowerCase() : '';
 
-        this.inputType = inputType; // the name of the form input class from Components/Form/index.js
-        this.values = values;       // available enums
-        this.locked = locked;       // not editable props
-        this.options = options;     // the elementProps override
+        this.inputType = inputType;
+        this.values = values;
+        this.locked = locked;
+        this.options = elementOptions;
 
-        this.debounceTime = 50;     //ms
+        this.debounceTime = 50;
 
-        this.targetElement = this.parent.element || false;
+        this.targetElement = parent?.element || false;
         this.elementTag = 'div';
         this.elementProps = {
             className: 'item'
         };
+    }
+
+    get dataType() {
+        return Array.isArray(this.value) ? 'array' : typeof this.value;
+    }
+
+    get dataTypeValues() {
+        return Array.isArray(this.values) ? 'array' : typeof this.values;
     }
 
     init() {
@@ -26,47 +44,37 @@ export default class Component {
     }
 
     render() {
-        // create the element
         this.element = document.createElement(this.elementTag);
-        // use element props
-        Object.keys(this.elementProps).forEach(prop => prop !== 'dataset' ? this.element[prop] = this.elementProps[prop] : null);
-        // all props except dataset
-        Object.keys(this.elementProps).forEach(prop => prop !== 'dataset' ? this.element[prop] = this.elementProps[prop] : null);
-        // only dataset
-        this.elementProps.dataset ? Object.keys(this.elementProps.dataset).forEach(dataKey => this.element.dataset[dataKey] = this.elementProps.dataset[dataKey]) : null;
+        Object.keys(this.elementProps).forEach(prop =>
+            prop !== 'dataset' ? this.element[prop] = this.elementProps[prop] : null
+        );
+        this.elementProps.dataset
+            ? Object.keys(this.elementProps.dataset).forEach(
+                dataKey => this.element.dataset[dataKey] = this.elementProps.dataset[dataKey]
+              )
+            : null;
     }
 
-    /*
     on(event, callback) {
-        return this.events.on(event, callback);
+        return this.parent?.events?.on(event, callback);
     }
 
     emit(event, ...args) {
-        return this.events.emit(event, ...args);
+        return this.parent?.events?.emit(event, ...args);
     }
-     */
 
-    // triggered from outside
     setValue(value) {
         this.element.value = value;
     }
 
     get value() {
-        return this.store[this.prop];
+        return this.store?.[this.prop];
     }
 
     set value(value) {
         clearTimeout(this.timer);
-        this.timer = setTimeout(() => this.store[this.prop] = value, this.debounceTime);
+        this.timer = setTimeout(() => {
+            if (this.store) this.store[this.prop] = value;
+        }, this.debounceTime);
     }
-
-    get dataType() {
-        return Array.isArray(this.value) ? 'array' : typeof this.value;
-    }
-
-    set dataType(val) {
-        // do nothing
-    }
-
-    get dataTypeValues() {
-        return Array.isArray(this.
+}
